6c9310366147a2a7318708d17ed121e931c10aab
Ludwig Langell <ludwig.langell@elev.ga.ntig.se>
2023-08-30 10:39:23 +0200

    FIrst commit

---
+++ Lucktext kanske gjord av ludwig langell/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine ("Hej hej vill du köra en lek");
+
+Console.ReadLine();
+ if (Console.ReadLine() == "Ja")
+ {
+Console.WriteLine ("Okej nämn ett battle royale spel där man kan bygga");
+Console.ReadLine (); 
+ }
+ if (Console.ReadLine () == "Fortnite" + "Forre")
+ {
+  Console.WriteLine ("Nämen wow du hade rätt woohoo.");
+  Console.ReadLine ();
+ }
+ 
